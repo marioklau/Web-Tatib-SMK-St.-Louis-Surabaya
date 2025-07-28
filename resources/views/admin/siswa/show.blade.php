5a692e8835c25d09f5acd4e71c69765c3ff8b5ff
Mario Klau <128314759+marioklau@users.noreply.github.com>
2025-07-28 14:25:36 +0700

    Update show.blade.php

--- resources/views/admin/siswa/show.blade.php
+++ resources/views/admin/siswa/show.blade.php
@@ -10,14 +10,14 @@
                 <img class="object-cover rounded-full" src="https://tailwindflex.com/public/images/user.png">
             </div>
             <div class="flex-grow text-center md:text-left">
-                @if ($siswa)
+                @isset ($siswa)
                     <p class="text-xl heading">{{ $siswa->nama_siswa }}</p>
                     <p class="">{{ $siswa->kelas->nama_kelas }}</p>
                     <p class="">{{ $siswa->nis }}</p>
                     <p class="">{{ $siswa->jenis_kelamin }}</p>
                 @else
                     <p class="text-gray-500">Data siswa tidak ditemukan.</p>
-                @endif
+                @endisset
             </div>
         </div>
     </div>
